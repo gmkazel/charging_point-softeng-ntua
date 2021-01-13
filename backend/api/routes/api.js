3bd3f5da484f0fe4e736e24e875c4d1932f543a5
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-01-13 16:10:53 +0200

    add userLogout endpoint

--- backend/api/routes/api.js
+++ backend/api/routes/api.js
@@ -4,6 +4,9 @@ const router = express.Router()
 const userLogin = require('../services/userLogin')
 router.post('/login', userLogin)
 
+const userLogout = require('../services/userLogout')
+router.post('/login', userLogout)
+
 const admin = require('./admin/admin')
 router.use('/admin', admin)
 
