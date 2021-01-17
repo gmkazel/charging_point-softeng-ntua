2c81ee0e24e9023e0701c6e2b76c8bec5c050747
Giannis K <gkazel@outlook.com>
2021-01-17 13:36:03 +0200

    renamed front-end

--- backend/api/services/healthcheck.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-module.exports = (req, res, next) => {
-  if (mongoose.connection.readyState === 1) { res.send({ status: 'OK' }) } else {
-    res.send({ status: 'failed' })
-  }
-  next()
-}
