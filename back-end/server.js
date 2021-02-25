f5191973a8cd57cae01aa52bde721572849ea5a5
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-02-25 19:29:26 +0200

    server maintenance
    * modified dependencies for testing capabillity
    * detached database connection to outside file
    * fixed sequence of server and database opening

--- back-end/server.js
+++ back-end/server.js
@@ -2,19 +2,26 @@ const config = require('config')
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
+
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+  app.use(morgan('combined'))
+}
+
+require('./api/utils/initDB')()
 
 app.use(bodyParser.json()) // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })) // support encoded bodies
 
 const api = require('./api/routes/api')
-
-mongoose.connect(config.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
-  console.log('Connected to database')
-})
-
-app.listen(config.PORT, function () {
-  console.log('Server is listening at port ' + config.PORT)
-})
-
 app.use(config.BASE_URL, api)
+
+mongoose.connection.on('connected', () => {
+  app.listen(config.PORT, function () {
+    console.log('Server is listening at port ' + config.PORT)
+    app.emit('appStarted')
+  })
+})
+
+module.exports = app
