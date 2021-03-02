89887a2ce777ed831563115ed0812da1c4e0392c
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-03-02 17:14:40 +0200

    refactor: station endpoints
    Put all endopoint handlers of stationmod directory to a single
    class

--- back-end/api/routes/stationmod.js
+++ back-end/api/routes/stationmod.js
@@ -2,16 +2,15 @@ const express = require('express')
 const router = express.Router()
 const verifyStationOwner = require('../services/verifyStationOwner')
 
-const addReview = require('../services/addReview')
-router.post('/addreview/:userId/:stationId', addReview)
+const StationEndpoint = require('../services/stationEndpoint')
+const stationEndpoint = new StationEndpoint()
 
-const addStation = require('../services/addStation')
-router.post('/add/:userId', verifyStationOwner, addStation)
+router.post('/addreview/:userId/:stationId', stationEndpoint.addReview)
 
-const editStation = require('../services/editStation')
-router.post('/edit/:userId/:stationId', verifyStationOwner, editStation)
+router.post('/add/:userId', verifyStationOwner, stationEndpoint.addStation)
 
-const deleteStation = require('../services/deleteStation')
-router.post('/delete/:userId/:stationId', verifyStationOwner, deleteStation)
+router.post('/edit/:userId/:stationId', verifyStationOwner, stationEndpoint.editStation)
+
+router.post('/delete/:userId/:stationId', verifyStationOwner, stationEndpoint.deleteStation)
 
 module.exports = router
---
+++ back-end/api/services/stationEndpoint.js
@@ -0,0 +1,132 @@
+const StationService = require('./stationService')
+const mongoose = require('mongoose')
+
+const stationService = new StationService()
+
+module.exports = class stationEndpoint {
+  async addStation (req, res, next) {
+    const userId = await req.params.userId
+    const cand = await req.body
+
+    const check = (variable) => {
+      return (typeof (variable) !== 'undefined' && variable !== null)
+    }
+
+    if (!(check(cand) && check(userId))) {
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+
+    try {
+      const result = await stationService.add(userId, cand)
+      res.send(result)
+      next()
+    } catch (err) {
+      console.log(err)
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+    next()
+  }
+
+  async deleteStation (req, res, next) {
+    const userId = await req.params.userId
+    const stationId = await req.params.stationId
+
+    const check = (variable) => {
+      return (typeof (variable) !== 'undefined' && variable !== null)
+    }
+
+    if (!(check(stationId) && check(userId))) {
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+
+    if (!stationService.ownedBy(userId, stationId)) {
+      res.status(401)
+      res.send()
+      return res.next('routes')
+    }
+
+    try {
+      const result = await stationService.delete(userId, stationId)
+      res.send({ deleted: result })
+      next()
+    } catch (err) {
+      console.log(err)
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+    next()
+  }
+
+  async editStation (req, res, next) {
+    const userId = await req.params.userId
+    const stationId = await req.params.stationId
+    const cand = await req.body
+
+    const check = (variable) => {
+      return (typeof (variable) !== 'undefined' && variable !== null)
+    }
+
+    if (!(check(cand) && check(stationId) && check(userId))) {
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+
+    if (!await stationService.ownedBy(userId, stationId)) {
+      res.status(401)
+      res.send()
+      return next('routes')
+    }
+
+    try {
+      const result = await stationService.edit(stationId, cand)
+      res.send(result)
+      next()
+    } catch (err) {
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+    next()
+  }
+
+  async addReview (req, res, next) {
+    const userId = await req.params.userId
+    const stationId = await req.params.stationId
+
+    const check = (variable) => {
+      return (typeof (variable) !== 'undefined' && variable !== null)
+    }
+
+    if (!(check(req.body) && check(stationId) && check(userId))) {
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+
+    const review = {
+      date: req.body.date,
+      by: mongoose.Types.ObjectId(userId),
+      rating: req.body.rating,
+      comment: req.body.comment
+    }
+    try {
+      const result = await stationService.addReview(review, stationId)
+      res.send({ added: result })
+      next()
+    } catch (err) {
+      console.log(err)
+      res.status(400)
+      res.send()
+      return next('route')
+    }
+    next()
+  }
+}
