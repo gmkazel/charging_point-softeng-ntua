f2a3b4dc7a77f60fee68ccdbde3061c4a70abc29
Giortzis Giannis <giortzis.giannis@gmail.com>
2021-03-03 23:18:19 +0200

    bug fix on sessionsupd
    add updates on point vehicle and user models
    when adding a session that refers to them

--- back-end/api/services/csvService.js
+++ back-end/api/services/csvService.js
@@ -1,5 +1,9 @@
+const mongoose = require('mongoose')
 const csv = require('csvtojson')
 const Session = require('../models/Session')
+const pointModel = require('../models/Point')
+const vehicleModel = require('../models/Vehicle')
+const userModel = require('../models/User')
 
 const upload = async (req, res) => {
   if (req.file[0] === undefined) {
@@ -12,6 +16,19 @@ const upload = async (req, res) => {
         if (err) {
           res.status(400).send(err)
         } else {
+          // change linked data
+          for (let i = 0; i < jsonArrayObj.length; i++) {
+            console.log(jsonArrayObj[i])
+            pointModel.findByIdAndUpdate(jsonArrayObj[i].point, { $push: { sessions: mongoose.Types.ObjectId(jsonArrayObj[i]._id) } }, (err) => {
+              if (err) console.log(err)
+            })
+            vehicleModel.findByIdAndUpdate(jsonArrayObj[i].car, { $push: { sessions: mongoose.Types.ObjectId(jsonArrayObj[i]._id) } }, (err) => {
+              if (err) console.log(err)
+            })
+            userModel.findByIdAndUpdate(jsonArrayObj[i].energy_provider_used, { $push: { electricalCompanyOperatorSessions: mongoose.Types.ObjectId(jsonArrayObj[i]._id) } }, (err) => {
+              if (err) console.log(err)
+            })
+          }
           Session.find()
             .then(data => {
               res.send({
