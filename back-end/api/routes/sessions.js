19b6781e148790a90f74fbba2f46037329f012dd
KonstantinosRiz <rizavask@gmail.com>
2021-03-04 20:15:11 +0200

    Add 4 requested endpoints
    Complete sessionService file.

--- back-end/api/routes/sessions.js
+++ back-end/api/routes/sessions.js
@@ -11,46 +11,46 @@ router.get('/SessionsPerPoint/:pointID/:yyyymmdd_from/:yyyymmdd_to', async (req,
     const endDate = req.params.yyyymmdd_to
 
     const result = await myService.getSessionsPerPoint(pointId, startDate, endDate)
-    // console.log(result)
+
     res.send(result)
   } catch (err) {
     console.log(err)
   }
 })
 
-router.get('/SessionsPerStation/:stationID/:yyyymmdd_from/:yyyymmdd_to', (req, res, next) => {
+router.get('/SessionsPerStation/:stationID/:yyyymmdd_from/:yyyymmdd_to', async (req, res, next) => {
   try {
     const stationId = req.params.stationID
     const startDate = req.params.yyyymmdd_from
     const endDate = req.params.yyyymmdd_to
 
-    const result = myService.getSessionsPerStation(stationId, startDate, endDate)
+    const result = await myService.getSessionsPerStation(stationId, startDate, endDate)
     res.send(result)
   } catch (err) {
     console.log(err)
   }
 })
 
-router.get('/SessionsPerEV/:vehicleID/:yyyymmdd_from/:yyyymmdd_to', (req, res, next) => {
+router.get('/SessionsPerEV/:vehicleID/:yyyymmdd_from/:yyyymmdd_to', async (req, res, next) => {
   try {
     const vehicleId = req.params.vehicleID
     const startDate = req.params.yyyymmdd_from
     const endDate = req.params.yyyymmdd_to
 
-    const result = myService.getSessionsPerEV(vehicleId, startDate, endDate)
+    const result = await myService.getSessionsPerEV(vehicleId, startDate, endDate)
     res.send(result)
   } catch (err) {
     console.log(err)
   }
 })
 
-router.get('/SessionsPerProvider/:providerID/:yyyymmdd_from/:yyyymmdd_to', (req, res, next) => {
+router.get('/SessionsPerProvider/:providerID/:yyyymmdd_from/:yyyymmdd_to', async (req, res, next) => {
   try {
     const providerId = req.params.providerID
     const startDate = req.params.yyyymmdd_from
     const endDate = req.params.yyyymmdd_to
 
-    const result = myService.getSessionsPerProvider(providerId, startDate, endDate)
+    const result = await myService.getSessionsPerProvider(providerId, startDate, endDate)
     res.send(result)
   } catch (err) {
     console.log(err)
