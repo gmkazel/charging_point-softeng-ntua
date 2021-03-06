616a11c782e6b7851e30401ef0475486bd165a9e
Giortzis Giannis <giortzis.giannis@gmail.com>
2021-03-06 16:36:29 +0200

    add extra cli commands to reflect backend
    * add estimated time call
    * add kilometers driver call
    * add periodic bill call

---
+++ cli-client/ev_group32/src/commands/periodicBill.js
@@ -0,0 +1,45 @@
+/* eslint-disable unicorn/filename-case */
+/* eslint-disable no-console */
+/* eslint-disable node/no-unpublished-require */
+const {Command, flags} = require('@oclif/command')
+
+const https = require('https')
+const axios = require('axios')
+const chalk = require('chalk')
+const config = require('config')
+
+axios.defaults.httpsAgent = new https.Agent()
+
+class periodicBill extends Command {
+  async run() {
+    try {
+      const {flags} = this.parse(periodicBill)
+      const status = await axios.get(`${config.BASE_URL}/periodicBill/${flags.ev}/${flags.datefrom}/${flags.dateto}`)
+      console.log(status.data)
+    } catch (error) {
+      console.error(chalk.red(error))
+    }
+  }
+}
+
+periodicBill.flags = {
+  format: flags.string({
+    options: ['json', 'csv'],
+    required: true,
+    default: 'json',
+  }),
+  ev: flags.string({
+    required: true,
+    description: 'the id of the car to search',
+  }),
+  datefrom: flags.string({
+    required: true,
+  }),
+  dateto: flags.string({
+    required: true,
+  }),
+}
+
+periodicBill.description = 'return total bill for all charging sessions in a certain period'
+
+module.exports = periodicBill
