501dc05288db762829b8086f064e992429f08b55
Giannis K <gkazel@outlook.com>
2021-03-07 22:28:28 +0200

    Update createUsers and createStation in order to create more stations per station owner

--- back-end/api/utils/createStation.js
+++ back-end/api/utils/createStation.js
@@ -6,42 +6,47 @@ const CreatePoints = require('./createPoints')
 const config = require('config')
 const createPoints = new CreatePoints()
 
-module.exports = async () => {
+module.exports = async (count) => {
   const userCount = config.dummyElectricalOperatorsCount
-  const random = getRandomInt(userCount)
-  const randUser = await userModel.findOne({ account_type: 'electricalCompanyOperator' }, 'username _id').skip(random)
-  const newStation = {
-    name: faker.fake('{{address.streetAddress}}, {{address.city}}'),
-    address: faker.fake('{{address.streetAddress}}, {{address.city}}'),
-    contact_info:
+  const finalStations = []
+  for (let i = 0; i < count; i++) {
+    const random = getRandomInt(userCount)
+    const randUser = await userModel.findOne({ account_type: 'electricalCompanyOperator' }, 'username _id').skip(random)
+    const newStation = {
+      name: faker.fake('{{address.streetAddress}}, {{address.city}}'),
+      address: faker.fake('{{address.streetAddress}}, {{address.city}}'),
+      contact_info:
       {
         email: faker.internet.email(),
         phone: [faker.phone.phoneNumber()]
       },
-    operator: faker.internet.userName(),
-    energy_provider: mongoose.Types.ObjectId(randUser._id)
+      operator: faker.internet.userName(),
+      energy_provider: mongoose.Types.ObjectId(randUser._id)
+    }
+
+    const res = await stationModel.create(newStation)
+    const stationID = res._id
+    const pointInstance = await createPoints.createPoints(getRndInteger(config.dummyMinPoints, config.dummyMaxPoints), stationID)
+    const pointsList = []
+
+    pointInstance.forEach((c) => {
+      pointsList.push(mongoose.Types.ObjectId(c._id))
+    })
+
+    const v = await stationModel.findByIdAndUpdate(stationID, {
+      points: pointsList
+    })
+
+    finalStations.push(v)
   }
 
-  const res = await stationModel.create(newStation)
-  const stationID = res._id
-  const pointInstance = await createPoints.createPoints(getRndInteger(config.dummyMinPoints, config.dummyMaxPoints), stationID)
-  const pointsList = []
-
-  pointInstance.forEach((c) => {
-    pointsList.push(mongoose.Types.ObjectId(c._id))
-  })
-
-  const v = await stationModel.findByIdAndUpdate(stationID, {
-    points: pointsList
-  })
-
-  return v
+  return finalStations
 }
 
-function getRandomInt (max) {
+function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max))
 }
 
-function getRndInteger (min, max) {
+function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min)) + min
 }
