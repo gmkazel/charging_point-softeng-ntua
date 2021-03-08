57688696a0b0a1e2f18c0510a96ca8981a7a12fb
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-03-08 14:56:05 +0200

    fix: server not closing gracefully

--- back-end/server.js
+++ back-end/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
-
+let server
 if (config.util.getEnv('NODE_ENV') !== 'test') {
   app.use(morgan('combined'))
 }
@@ -18,7 +18,7 @@ const api = require('./api/routes/api')
 app.use(config.BASE_URL, api)
 
 mongoose.connection.on('connected', () => {
-  app.listen(config.PORT, function () {
+  server = app.listen(config.PORT, function () {
     console.log('Server is listening at port ' + config.PORT)
     app.emit('appStarted')
   })
@@ -37,7 +37,7 @@ process.on('SIGINT', shutDown)
 
 function shutDown () {
   console.log('Received kill signal, shutting down gracefully')
-  app.close(() => {
+  server.close(() => {
     console.log('Closed out remaining connections')
     process.exit(0)
   })
