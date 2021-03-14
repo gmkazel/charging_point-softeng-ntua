018ad7ed835c3d1df79d4f7b97846989b5b1b193
Marios Kerasiotis <marioskerasiotis@gmail.com>
2021-03-14 13:49:29 +0200

    fix: 404 page

--- front-end/src/pages/404.js
+++ front-end/src/pages/404.js
@@ -4,17 +4,17 @@ import './404.css';
 
 const NotFoundPage = () => {
     return (
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="error-template">
+        <div className="container">
+            <div className="row">
+                <div className="col-md-12">
+                    <div className="error-template">
                         <h1>Oops!</h1>
                         <h2>404 Not Found</h2>
-                        <div class="error-details">
+                        <div className="error-details">
                             Sorry, an error has occured, requested page not found or permission not granted to access it!
                         </div>
-                        <div class="error-actions">
-                            <Link to="/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-home"></span>Take Me Home</Link>
+                        <div className="error-actions">
+                            <Link to="/" className="btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span>Take Me Home</Link>
                         </div>
                     </div>
                 </div>
--- front-end/src/pages/adminuser.js
+++ front-end/src/pages/adminuser.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import nickie from '../components/images/42.jpeg'
 
 const AdminPage = () => {
     return (
@@ -12,9 +13,12 @@ const AdminPage = () => {
                         <div className="error-details">
                             <p>Unfortunately, there are no pages for you on the website.</p>
                             <span>Please use our cli-client to access your priviliges.</span>
+                            <br/>
+                            <br/>
                         </div>
+                        <img  src={nickie} alt="42" width="25%"/>
                         <div className="error-actions">
-                            <Link to="/" className="btn btn-primary btn-lg"><span classNames="glyphicon glyphicon-home"></span>Take Me Home</Link>
+                            <Link to="/" className="btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span>Take Me Home</Link>
                         </div>
                     </div>
                 </div>
